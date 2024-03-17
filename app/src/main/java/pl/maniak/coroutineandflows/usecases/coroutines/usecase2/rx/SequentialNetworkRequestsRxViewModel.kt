17f44f39cf684f3ae60cb749ebe09bca6366fa57
Piotr Liszka <piotr.liszka.pl@gmail.com>
2024-03-17 10:55:49 +0100

    9. RxJava Implementation

--- app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase2/rx/SequentialNetworkRequestsRxViewModel.kt
+++ app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase2/rx/SequentialNetworkRequestsRxViewModel.kt
@@ -1,12 +1,42 @@
 package pl.maniak.coroutineandflows.usecases.coroutines.usecase2.rx
 
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import pl.maniak.coroutineandflows.base.BaseViewModel
 
 class SequentialNetworkRequestsRxViewModel(
     private val mockApi: RxMockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
-    fun perform2SequentialNetworkRequest() {
+    private val disposables = CompositeDisposable()
 
+    fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
+
+        mockApi.getRecentAndroidVersions()
+            .flatMap { androidVersions ->
+                val recentVersion = androidVersions.last()
+                mockApi.getAndroidVersionFeatures(recentVersion.apiLevel)
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onSuccess = { featureVersions ->
+                    uiState.value = UiState.Success(featureVersions)
+                },
+                onError = {
+                    uiState.value = UiState.Error("Network request failed")
+                }
+            )
+            .addTo(disposables)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        disposables.clear()
     }
 }
