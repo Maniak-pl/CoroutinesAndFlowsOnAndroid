a8a8dcae28c8873dbc54f6505ba02c632f8c4883
Piotr Liszka <piotr.liszka.pl@gmail.com>
2024-03-19 10:39:28 +0100

    17. Blocking VS Suspending

---
+++ app/src/main/java/pl/maniak/coroutineandflows/playground/fundamentals/7_suspending_coroutines.kt
@@ -0,0 +1,27 @@
+package pl.maniak.coroutineandflows.playground.fundamentals
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    println("Main starts")
+    joinAll(
+        async { suspendingCoroutine(1, 500) },
+        async { suspendingCoroutine(2, 300) },
+        async {
+            repeat(5) {
+                println("other task is working on ${Thread.currentThread().name}")
+                delay(100)
+            }
+        }
+    )
+    println("Main ends")
+}
+
+suspend fun suspendingCoroutine(number: Int, delay: Long) {
+    println("Coroutine $number starts work on ${Thread.currentThread().name}")
+    delay(delay)
+    println("Coroutine $number has finished on ${Thread.currentThread().name}")
+}
