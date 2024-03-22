1eb04778530138312702d93e63aa7904c39b8e4e
Piotr Liszka <piotr.liszka.pl@gmail.com>
2024-03-22 15:25:58 +0100

    34. Implementing UseCase#3 in a concurrent way

--- app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -1,6 +1,7 @@
 package pl.maniak.coroutineandflows.usecases.coroutines.usecase3
 
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import pl.maniak.coroutineandflows.base.BaseViewModel
 import pl.maniak.coroutineandflows.mock.MockApi
@@ -26,6 +27,30 @@ class PerformNetworkRequestsConcurrentlyViewModel(
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.value = UiState.Loading
+        val oreoFeaturesDeferred = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(27)
+        }
 
+        val pieFeaturesDeferred = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(28)
+        }
+
+        val android10FeaturesDeferred = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(29)
+        }
+
+        viewModelScope.launch {
+            try {
+                val versionFeatures = listOf(
+                    oreoFeaturesDeferred.await(),
+                    pieFeaturesDeferred.await(),
+                    android10FeaturesDeferred.await()
+                )
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network request failed")
+            }
+        }
     }
 }
