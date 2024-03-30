af962bd9c6f4e20cf4525d330a1ad1becd87db55
Piotr Liszka <piotr.liszka.pl@gmail.com>
2024-03-30 06:20:01 +0100

    40. Implementing retries

--- app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,39 @@
 package pl.maniak.coroutineandflows.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import pl.maniak.coroutineandflows.base.BaseViewModel
 import pl.maniak.coroutineandflows.mock.MockApi
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            val numberOfRetries = 2
+
+            try {
+                repeat(numberOfRetries) {
+                    try {
+                        loadRecentAndroidVersions()
+                        return@launch
+                    } catch (exception: Exception) {
+                        Timber.e(exception)
+                    }
+                }
+                loadRecentAndroidVersions()
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Network request failed")
+            }
+        }
     }
 
+    private suspend fun loadRecentAndroidVersions() {
+        val result = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(result)
+    }
 }
