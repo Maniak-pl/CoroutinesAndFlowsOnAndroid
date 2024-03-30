bda6d98ed1e35b9d062ff0fe993f05f358703adf
Piotr Liszka <piotr.liszka.pl@gmail.com>
2024-03-30 06:00:06 +0100

    39. Implementing a timeout

--- app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
+++ app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
@@ -1,14 +1,57 @@
 package pl.maniak.coroutineandflows.usecases.coroutines.usecase5
 
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 import pl.maniak.coroutineandflows.base.BaseViewModel
 import pl.maniak.coroutineandflows.mock.MockApi
+import timber.log.Timber
 
 class NetworkRequestWithTimeoutViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest(timeout: Long) {
+        uiState.value = UiState.Loading
 
+        usingWithTimeOut(timeout)
+        usingWithTimeOutOrNull(timeout)
     }
 
+    private fun usingWithTimeOut(timeout: Long) {
+        viewModelScope.launch {
+            try {
+                val result = withTimeout(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                uiState.value = UiState.Success(result)
+            } catch (timeoutCancellationException: TimeoutCancellationException) {
+                Timber.e(timeoutCancellationException)
+                uiState.value = UiState.Error("Network request timed out")
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Network request failed")
+            }
+        }
+    }
+
+    private fun usingWithTimeOutOrNull(timeout: Long) {
+        viewModelScope.launch {
+            try {
+                val result = withTimeoutOrNull(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                if (result != null) {
+                    uiState.value = UiState.Success(result)
+                } else {
+                    uiState.value = UiState.Error("Network request timed out or null result")
+                }
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Network request failed")
+            }
+        }
+    }
 }
