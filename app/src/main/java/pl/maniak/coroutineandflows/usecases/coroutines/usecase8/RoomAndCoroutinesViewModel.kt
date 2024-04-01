6a2588f5d2ad70903f48e82fbab65506b9c7106f
Piotr Liszka <piotr.liszka.pl@gmail.com>
2024-04-01 07:16:33 +0200

    48. Using Room with Coroutines Implementation

--- app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
+++ app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
@@ -1,7 +1,11 @@
 package pl.maniak.coroutineandflows.usecases.coroutines.usecase8
 
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import pl.maniak.coroutineandflows.base.BaseViewModel
 import pl.maniak.coroutineandflows.mock.MockApi
+import pl.maniak.coroutineandflows.usecases.coroutines.usecase8.DataSource.DATABASE
+import pl.maniak.coroutineandflows.usecases.coroutines.usecase8.DataSource.NETWORK
 
 class RoomAndCoroutinesViewModel(
     private val api: MockApi,
@@ -9,11 +13,33 @@ class RoomAndCoroutinesViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun loadData() {
+        uiState.value = UiState.Loading.LoadFromDb
 
+        viewModelScope.launch {
+            val localVersions = database.getAndroidVersions()
+            if (localVersions.isEmpty()) {
+                uiState.value = UiState.Error(DATABASE, "Database is empty")
+            } else {
+                uiState.value = UiState.Success(DATABASE, localVersions.mapToUiModelList())
+            }
+
+            uiState.value = UiState.Loading.LoadFromNetwork
+            try {
+                val recentVersions = api.getRecentAndroidVersions()
+                for (version in recentVersions) {
+                    database.insert(version.mapToEntity())
+                }
+                uiState.value = UiState.Success(NETWORK, recentVersions)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error(NETWORK, "Network request failed")
+            }
+        }
     }
 
     fun clearDatabase() {
-
+        viewModelScope.launch {
+            database.clear()
+        }
     }
 }
 
