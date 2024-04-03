6dffae59c714e23fc3c14bb0438b50599abc3f46
Piotr Liszka <piotr.liszka.pl@gmail.com>
2024-04-03 06:35:26 +0200

    52. Implementation running on Main Thread

--- app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/pl/maniak/coroutineandflows/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -1,10 +1,37 @@
 package pl.maniak.coroutineandflows.usecases.coroutines.usecase10
 
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import pl.maniak.coroutineandflows.base.BaseViewModel
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
 
     fun performCalculation(factorialOf: Int) {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            var result: BigInteger
+            val computationDuration = measureTimeMillis {
+                result = calculateFactorialOf(factorialOf)
+            }
+
+            var resultString: String
+            val stringConversionDuration = measureTimeMillis {
+                resultString = result.toString()
+            }
+
+            uiState.value =
+                UiState.Success(resultString, computationDuration, stringConversionDuration)
+        }
+    }
+
+    private fun calculateFactorialOf(number: Int): BigInteger {
+        var factorial = BigInteger.ONE
+        for (i in 1..number) {
+            factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+        }
+        return factorial
     }
 }
